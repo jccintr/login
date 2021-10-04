19155248696a6cf13c021b675b49283d689ec025
Julio Cesar <jccintr@gmail.com>
2021-10-04 14:59:47 -0300

    Tela Azul

--- App.js
+++ App.js
@@ -1,21 +1,370 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+//import { StatusBar } from 'expo-status-bar';
+import React, { useState, useEffect } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  TextInput,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
 
-export default function App() {
+// expo install expo-linear-gradient (For Expo Users)
+
+import { LinearGradient } from 'expo-linear-gradient';
+
+// npm install react-native-elements
+import { Icon } from 'react-native-elements';
+
+// https://fonts.google.com/specimen/Nunito+Sans
+import { useFonts } from 'expo-font';
+
+import NSLight from './assets/fonts/NunitoSans-Light.ttf';
+import NSRegular from './assets/fonts/NunitoSans-Regular.ttf';
+import NSBold from './assets/fonts/NunitoSans-Bold.ttf';
+import NSExtraBold from './assets/fonts/NunitoSans-ExtraBold.ttf';
+
+export default function LoginScreen3() {
+  const [loaded] = useFonts({
+    NSLight,
+    NSRegular,
+    NSBold,
+    NSExtraBold,
+  });
+
+  const [activeTab, setActiveTab] = useState('Login');
+
+  useEffect(function () {
+    StatusBar.setBarStyle('light-content', true);
+  }, []);
+
+  function switchTab() {
+    if (activeTab === 'Login') {
+      setActiveTab('Register');
+    } else {
+      setActiveTab('Login');
+    }
+  }
+
+  function Login() {
+    const [showLoginPassword, setShowLoginPassword] = useState(false);
+
+    return (
+      <View style={{ marginTop: 10 }}>
+        <View style={styles.inputView}>
+          <Icon
+            style={{ paddingHorizontal: 4 }}
+            name='envelope'
+            type='font-awesome'
+            color='#fff'
+            size={22}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder='Email'
+            placeholderTextColor='#f1f2f6'
+            keyboardType='email-address'
+            textContentType='emailAddress'
+            autoCapitalize={false}
+            autoCompleteType='email'
+            returnKeyType='next'
+          />
+        </View>
+        <View style={styles.inputView}>
+          <Icon
+            style={{ paddingHorizontal: 4 }}
+            name='key'
+            type='font-awesome-5'
+            color='#fff'
+            size={22}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder='Senha'
+            placeholderTextColor='#f1f2f6'
+            secureTextEntry={!showLoginPassword}
+            textContentType='password'
+            returnKeyType='done'
+          />
+          <TouchableOpacity
+            style={{ paddingVertical: 4 }}
+            onPress={() => {
+              setShowLoginPassword(!showLoginPassword);
+            }}
+          >
+            <Icon
+              style={{ paddingHorizontal: 4 }}
+              name='eye'
+              type='font-awesome'
+              color='#fff'
+              size={22}
+            />
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Entrar</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={styles.forgotPasswordText}>Esqueceu a senha ?</Text>
+        </TouchableOpacity>
+       
+      </View>
+    );
+  }
+
+  function Register() {
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+    const [showRegisterPassword, setShowRegisterPassword] = useState(false);
+    return (
+      <View style={{ marginTop: 10 }}>
+        <View style={styles.inputView}>
+          <Icon
+            style={{ paddingHorizontal: 4, width: 30 }}
+            name='user'
+            type='font-awesome'
+            color='#fff'
+            size={22}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder='Nome Completo'
+            placeholderTextColor='#f1f2f6'
+            textContentType='name'
+            autoCompleteType='name'
+            returnKeyType='next'
+          />
+        </View>
+        <View style={styles.inputView}>
+          <Icon
+            style={{ paddingHorizontal: 4, width: 30 }}
+            name='envelope'
+            type='font-awesome'
+            color='#fff'
+            size={22}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder='Email'
+            placeholderTextColor='#f1f2f6'
+            keyboardType='email-address'
+            textContentType='emailAddress'
+            autoCapitalize={false}
+            autoCompleteType='email'
+            returnKeyType='next'
+          />
+        </View>
+        <View style={styles.inputView}>
+          <Icon
+            style={{ paddingHorizontal: 4, width: 30 }}
+            name='phone'
+            type='font-awesome'
+            color='#fff'
+            size={22}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder='Telefone'
+            placeholderTextColor='#f1f2f6'
+            keyboardType='phone-pad'
+            returnKeyType='next'
+          />
+        </View>
+        <View style={styles.inputView}>
+          <Icon
+            style={{ paddingHorizontal: 4, width: 30 }}
+            name='key'
+            type='font-awesome-5'
+            color='#fff'
+            size={22}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder='Senha'
+            placeholderTextColor='#f1f2f6'
+            secureTextEntry={!showRegisterPassword}
+            textContentType='password'
+            returnKeyType='done'
+          />
+          <TouchableOpacity
+            style={{ paddingVertical: 4 }}
+            onPress={() => {
+              setShowRegisterPassword(!showRegisterPassword);
+            }}
+          >
+            <Icon
+              style={{ paddingHorizontal: 4 }}
+              name='eye'
+              type='font-awesome'
+              color='#fff'
+              size={22}
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={styles.inputView}>
+          <Icon
+            style={{ paddingHorizontal: 4, width: 30 }}
+            name='key'
+            type='font-awesome-5'
+            color='#fff'
+            size={22}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder='Confirme a Senha'
+            placeholderTextColor='#f1f2f6'
+            secureTextEntry={!showConfirmPassword}
+            textContentType='password'
+            returnKeyType='done'
+          />
+          <TouchableOpacity
+            style={{ paddingVertical: 4 }}
+            onPress={() => {
+              setShowConfirmPassword(!showConfirmPassword);
+            }}
+          >
+            <Icon
+              style={{ paddingHorizontal: 4 }}
+              name='eye'
+              type='font-awesome'
+              color='#fff'
+              size={22}
+            />
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Cadastrar</Text>
+        </TouchableOpacity>
+        
+      </View>
+    );
+  }
+
+  if (!loaded) {
+    return (
+      <View>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <LinearGradient colors={['#3A7BD5', '#00D2FF']} style={styles.container}>
+        <Text style={styles.welcomeText}>
+          {activeTab === 'Login' ? 'Entrar' : 'Cadastre-se'}
+        </Text>
+        <View style={styles.switchTabsView}>
+          <TouchableOpacity
+            style={{
+              borderBottomWidth: activeTab === 'Login' ? 4 : 0,
+              borderBottomColor: '#fff',
+              paddingHorizontal: 4,
+              marginRight: 14,
+            }}
+            onPress={() => switchTab()}
+          >
+            <Text style={styles.switchText}>Login</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              borderBottomWidth: activeTab === 'Register' ? 4 : 0,
+              borderBottomColor: '#fff',
+              paddingHorizontal: 4,
+              marginRight: 14,
+            }}
+            onPress={() => switchTab()}
+          >
+            <Text style={styles.switchText}>Novo Usuário</Text>
+          </TouchableOpacity>
+        </View>
+        {activeTab === 'Login' ? <Login /> : <Register />}
+      </LinearGradient>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 40,
+  },
+  welcomeText: {
+    alignSelf: 'center',
+    fontSize: 40,
+    fontFamily: 'NSLight',
+    marginTop: 10,
+    color: '#fff',
+  },
+  switchTabsView: {
+    display: 'flex',
+    flexDirection: 'row',
+    paddingHorizontal: 20,
+    marginTop: 20,
+  },
+  switchText: {
+    padding: 2,
+    fontSize: 20,
+    color: '#fff',
+    fontFamily: 'NSExtraBold',
+  },
+  inputView: {
+    height: 40,
+    borderBottomWidth: 1,
+    borderBottomColor: '#fff',
+    marginTop: 10,
+    marginHorizontal: 20,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  input: {
+    flex: 1,
+    height: 40,
+    fontSize: 16,
+    fontFamily: 'NSLight',
+    paddingHorizontal: 4,
+    color: '#fff',
+  },
+  button: {
+    marginHorizontal: 20,
+    backgroundColor: '#fafafa',
+    marginTop: 12,
+    paddingVertical: 10,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  buttonText: { fontFamily: 'NSRegular', fontSize: 16, color: '#3A7BD5',fontWeight: 'bold' },
+  forgotPasswordText: {
+    marginHorizontal: 20,
+    marginTop: 20,
+    alignSelf: 'flex-end',
+    color: '#fff',
+    fontSize: 18,
+    fontFamily: 'NSBold',
+  },
+  socialLoginView: {
+    marginTop: 40,
+    marginHorizontal: 20,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  socialLoginTouchable: {
     backgroundColor: '#fff',
+    width: 40,
+    height: 40,
+    borderRadius: 100,
     alignItems: 'center',
     justifyContent: 'center',
+    marginHorizontal: 8,
   },
 });
